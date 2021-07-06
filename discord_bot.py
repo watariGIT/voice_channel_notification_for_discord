a388a9010e03e4f0d4a216af3cec0f00f25eab3b
Naoya Ishiwatari <nishiwat@yahoo-corp.jp>
2021-07-06 23:31:46 +0900

    msgに時刻を追加

--- discord_bot.py
+++ discord_bot.py
@@ -1,5 +1,6 @@
 import discord
 import os 
+from datetime import datetime as dt
 
 TOKEN = os.environ.get('DISCORD_BOT_TOKEN')
 SEND_CH_ID = int(os.environ.get('SEND_CH_ID', 0))
@@ -15,13 +16,16 @@ async def on_ready():
 @client.event
 async def on_voice_state_update(member, before, after):
     send_channel = client.get_channel(SEND_CH_ID)
+    
     if  after.channel is not None and after.channel.id== VOICE_CH_ID:
-        msg = f'`{member.name}` が参加しました。'
+        msg = f'[{dt.now().strftime("%Y/%m/%d %H:%M:%S")}] `{member.name}` が参加しました。'
+        
         print(msg)
         await send_channel.send(msg)
     elif before.channel is not None and before.channel.id == VOICE_CH_ID:
-        msg = f'`{member.name}` が退出しました。'
+        msg = f'[{dt.now().strftime("%Y/%m/%d %H:%M:%S")}] `{member.name}` が退出しました。'
+        
         print(msg)
         await send_channel.send(msg)
-
+    
 client.run(TOKEN)
