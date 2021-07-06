f72904d0e3bf5ce8aba8e1e081bc36513dd5237c
Naoya Ishiwatari <nishiwat@yahoo-corp.jp>
2021-07-06 23:32:28 +0900

    状態変化時にも通知してしまうバグへの対応

--- discord_bot.py
+++ discord_bot.py
@@ -16,16 +16,16 @@ async def on_ready():
 @client.event
 async def on_voice_state_update(member, before, after):
     send_channel = client.get_channel(SEND_CH_ID)
-    
-    if  after.channel is not None and after.channel.id== VOICE_CH_ID:
-        msg = f'[{dt.now().strftime("%Y/%m/%d %H:%M:%S")}] `{member.name}` が参加しました。'
-        
-        print(msg)
-        await send_channel.send(msg)
-    elif before.channel is not None and before.channel.id == VOICE_CH_ID:
-        msg = f'[{dt.now().strftime("%Y/%m/%d %H:%M:%S")}] `{member.name}` が退出しました。'
-        
-        print(msg)
-        await send_channel.send(msg)
+    if after.channel != before.channel :
+        if  after.channel is not None and after.channel.id== VOICE_CH_ID:
+            msg = f'[{dt.now().strftime("%Y/%m/%d %H:%M:%S")}] `{member.name}` が参加しました。'
+            
+            print(msg)
+            await send_channel.send(msg)
+        elif before.channel is not None and before.channel.id == VOICE_CH_ID:
+            msg = f'[{dt.now().strftime("%Y/%m/%d %H:%M:%S")}] `{member.name}` が退出しました。'
+            
+            print(msg)
+            await send_channel.send(msg)
     
 client.run(TOKEN)
