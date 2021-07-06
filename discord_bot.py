00febc2fbd45690cfe90fbe8e6d4179c48eb7ee0
Naoya Ishiwatari <nishiwat@yahoo-corp.jp>
2021-07-06 22:21:30 +0900

    logの修正

--- discord_bot.py
+++ discord_bot.py
@@ -6,7 +6,6 @@ SEND_CH_ID = int(os.environ.get('SEND_CH_ID', 0))
 VOICE_CH_ID = int(os.environ.get('VOICE_CH_ID', 0))
 
 # 接続に必要なオブジェクトを生成
-print(TOKEN)
 client = discord.Client()
 
 @client.event
@@ -17,10 +16,12 @@ async def on_ready():
 async def on_voice_state_update(member, before, after):
     send_channel = client.get_channel(SEND_CH_ID)
     if  after.channel is not None and after.channel.id== VOICE_CH_ID:
-        msg = f'{member.name} が参加しました。'
+        msg = f'`{member.name}` が参加しました。'
+        print(msg)
         await send_channel.send(msg)
     elif before.channel is not None and before.channel.id == VOICE_CH_ID:
-        msg = f'{member.name} が退出しました。'
+        msg = f'`{member.name}` が退出しました。'
+        print(msg)
         await send_channel.send(msg)
 
 client.run(TOKEN)
