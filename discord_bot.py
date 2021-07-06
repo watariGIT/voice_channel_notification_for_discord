ff9c25ea8c617ce62674135b19b14a9daadecd0e
Naoya Ishiwatari <nishiwat@yahoo-corp.jp>
2021-07-06 23:39:38 +0900

    通話時間を表示

--- discord_bot.py
+++ discord_bot.py
@@ -6,8 +6,9 @@ TOKEN = os.environ.get('DISCORD_BOT_TOKEN')
 SEND_CH_ID = int(os.environ.get('SEND_CH_ID', 0))
 VOICE_CH_ID = int(os.environ.get('VOICE_CH_ID', 0))
 
-# 接続に必要なオブジェクトを生成
+
 client = discord.Client()
+member_time ={}
 
 @client.event
 async def on_ready():
@@ -18,12 +19,15 @@ async def on_voice_state_update(member, before, after):
     send_channel = client.get_channel(SEND_CH_ID)
     if after.channel != before.channel :
         if  after.channel is not None and after.channel.id== VOICE_CH_ID:
-            msg = f'[{dt.now().strftime("%Y/%m/%d %H:%M:%S")}] `{member.name}` が参加しました。'
+            member_time[member.id]=dt.now()
+            msg = f'【{dt.now().strftime("%Y/%m/%d %H:%M:%S")}】 `{member.name}` が参加しました。'
             
             print(msg)
             await send_channel.send(msg)
         elif before.channel is not None and before.channel.id == VOICE_CH_ID:
-            msg = f'[{dt.now().strftime("%Y/%m/%d %H:%M:%S")}] `{member.name}` が退出しました。'
+            duration_time =  dt.now() - member_time[member.id]
+            msg = f'【{dt.now().strftime("%Y/%m/%d %H:%M:%S")}】 `{member.name}` が退出しました。'
+            msg += f'【通話時間 {duration_time.strftime("%H:%M:%S")}】'
             
             print(msg)
             await send_channel.send(msg)
