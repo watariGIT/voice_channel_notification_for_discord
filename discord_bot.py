d96914ae7dd4fc99d57304656662eac708f73c84
Naoya Ishiwatari <nishiwat@yahoo-corp.jp>
2021-07-06 23:49:01 +0900

    日本時刻を表示するように修正

--- discord_bot.py
+++ discord_bot.py
@@ -1,6 +1,7 @@
 import discord
 import os 
 from datetime import datetime as dt
+import pytz
 
 TOKEN = os.environ.get('DISCORD_BOT_TOKEN')
 SEND_CH_ID = int(os.environ.get('SEND_CH_ID', 0))
@@ -18,17 +19,18 @@ async def on_ready():
 async def on_voice_state_update(member, before, after):
     send_channel = client.get_channel(SEND_CH_ID)
     if after.channel != before.channel :
+        now = dt.now(pytz.timezone('Asia/Tokyo'))
+        
         if  after.channel is not None and after.channel.id== VOICE_CH_ID:
-            member_time[member.id]=dt.now()
-            msg = f'【{dt.now().strftime("%Y/%m/%d %H:%M:%S")}】 `{member.name}` が参加しました。'
-            
+            member_time[member.id]= now
+            msg = f'【{now.strftime("%Y/%m/%d %H:%M:%S")}】 `{member.name}` が参加しました。'
             print(msg)
             await send_channel.send(msg)
-        elif before.channel is not None and before.channel.id == VOICE_CH_ID:
-            duration_time =  dt.now() - member_time[member.id]
-            msg = f'【{dt.now().strftime("%Y/%m/%d %H:%M:%S")}】 `{member.name}` が退出しました。'
-            msg += f'【通話時間 {duration_time.strftime("%H:%M:%S")}】'
             
+        elif before.channel is not None and before.channel.id == VOICE_CH_ID:
+            duration_time =  now - member_time[member.id]
+            msg = f'【{now.strftime("%Y/%m/%d %H:%M:%S")}】 `{member.name}` が退出しました。'
+            msg = msg + f'【通話時間 {duration_time.strftime("%H:%M:%S")}】'
             print(msg)
             await send_channel.send(msg)
     
